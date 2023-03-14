f505f59af082b05f9c9d66ae9c46f8552dc74a0d
sysbot <systecinfo_cl@criptext.com>
2023-03-14 00:49:45 -0300

    modify models.py

--- blog_app/models.py
+++ blog_app/models.py
@@ -4,25 +4,18 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class Category(models.Model):
-    short_name = models.CharField(max_length=100)
-    description = models.TextField()
+class Sismo(models.Model):
+    local_date_cl = models.CharField(max_length=100)
+    place = models.TextField()
+    fecha_utc = models.TextField()
+    earthquake_latitude = models.TextField()
+    earthquake_longitude = models.TextField()
+    earthquake_deep = models.TextField()
+    magnitude = models.TextField()
 
     class Meta:
-        verbose_name_plural = "categories"
+        verbose_name_plural = "sismos"
 
     def __str__(self): """
         return self.short_name """
 
-class Post(models.Model):
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-    title = models.CharField(max_length=100)
-    body = models.TextField()
-    created_at = models.DateTimeField(default=timezone.now)
-    """ categories = models.ManyToManyField(Category) """
-
-    class Meta:
-        ordering = ['-created_at']
-
-    def __str__(self):
-        return f'"{self.title}" by: {self.author.username}'
